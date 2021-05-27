db92e65915d0720b51f5f6dfa532b21653346c71
Albert Araque <aaraquedesarrollo@gmail.com>
2021-05-27 13:18:37 +0200

    Funciones esItaliano y printarPorConsola

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+function esItaliano (apellido) {
+  return apellido.endsWith("i");
+}
+
+function printarPorConsola(boolean) {
+  if (boolean) {
+    console.log("Este apellido tiene muchas probabilidades de ser italiano")
+  }
+  else {
+    console.log("Este apellido podría ser de cualquier país");
+  }
+}
+
+const apellido1 = "Gonzalez";
+const apellido2 = "Falciani";
+
+printarPorConsola(esItaliano(apellido1));
+printarPorConsola(esItaliano(apellido2));
